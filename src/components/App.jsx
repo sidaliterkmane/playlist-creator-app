c746bb3a620246c54799902da17d1a53e067c305
Sid Ali Terkmane <sidterkmane@hotmail.com>
2022-07-24 15:42:59 -0400

    implemented spotify API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,26 +8,21 @@ import Playlist from './Playlist/Playlist'
 
 import logo from '../assets/logo.png'
 
+import Spotify from '../util/Spotify';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [
-        { name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
-        { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
-        { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
-      ],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [
-        { name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4 },
-        { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5 },
-        { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }
-      ]
+      playlistTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -54,6 +49,18 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
+  }
+
+  search(searchTerm) {
+    Spotify.search(searchTerm).then(searchResults => {
+      this.setState({ searchResults: searchResults })
+    });
   }
 
   render() {
@@ -62,12 +69,12 @@ class App extends React.Component {
       <div className='body-container'>
         <div className="body">
           <div className="logo">
-            <img src={logo} />
+            <img src={ logo } />
             <h1>Spoti<span className="highlight">list</span></h1>
           </div>
 
           <div className="App">
-            <SearchBar />
+            <SearchBar onSearch={this.search}/>
             <div className="App-playlist">
               <SearchResults
                 searchResults={this.state.searchResults}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom'
 import './SearchBar.scss'
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    }
+
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+
+  search() {
+    this.props.onSearch(this.state.searchTerm)
+  }
+
+  handleTermChange(event) {
+    this.setState({searchTerm: event.target.value})
+  }
+  
   render() {
-    
     return (
       <div className="SearchBar">
-        <input placeholder="Enter a song, album, artist" />
-        <button className="SearchButton">Search</button>
+        <input placeholder="Enter a song, album, artist" onChange={this.handleTermChange}/>
+        <button className="SearchButton" onClick={this.search}>Search</button>
       </div>
     )
   }
